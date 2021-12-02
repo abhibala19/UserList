58c85d1b85f167bfdbd34b1991d7cb2e4acf4ea1
abhibala19 <abhinaya.2595@gmail.com>
2021-12-02 21:38:58 +0530

    dob is fixed

--- userlist/src/User.js
+++ userlist/src/User.js
@@ -1,84 +1,85 @@
-    import React, { Component } from 'react'
-    import UserLists from './UserList'
-    import "./AppStyle.css"
-    import Calendar from 'react-calendar'
-    class User extends Component {
-        constructor(props) {
-            super(props)
-
-            this.state = {
-                userList: [],
-                userObj: {
-                    DateOfBirth:Date
-                }
-                
-            }
-        }
-        // clearField = (event) => {
-        //     Array.from(event.target).forEach((e) => (e.value = ""));
-        // }
-
-        clickHandler = (event) => {
-            this.state.userList.push(this.state.userObj);
-            this.setState({
-                // userObj: { firstName: "", lastName: "", Email: "", DateOfBirth: "" }
-                userObj: {}
-            })
-
-        }
-        handleDateChange =(event) => {
-            this.state.userObj.DateOfBirth = event
-        }
-        handleChange = (event, name) => {
-            this.state.userObj[name] = event
-        }
-        render() {
-            return (
-                <div >
-                    <div>
-                        <label>FirstName  </label>
-                        <input type="text" name="firstName"
-                            id="inputField"
-                            value={this.state.userObj.firstName}
-                            onChange={(event) => { this.handleChange(event.target.value, "firstName") }}></input>
-                        <label>LastName  </label>
-                        <input type="text" name="lastName"
-                            id="inputField"
-                            value={this.state.userObj.lastName}
-                            onChange={(event) => { this.handleChange(event.target.value, "lastName") }}></input>
-                    </div><br />
-                    <div>
-
-                        <label>EmailId</label>
-                        <input type="email" name="Email"
-                            id="inputField"
-                            value={this.state.userObj.Email}
-                            onChange={(event) => { this.handleChange(event.target.value, "Email") }}></input>
+import React, { Component } from 'react'
+import UserLists from './UserList'
+import "./AppStyle.css"
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 
-                        <label>DateOfBirth  </label>
-                        <Calendar 
-                        id="inputField"
-                        value={this.state.userOfObj.DateOfBirth ?"":this.state.userOfObj.DateOfBirth}
-                        onChange={ (event) => { this.handleDateChange(event.target.value)}} />
-          {/* <input type="text" name="DateOfBirth"
-                            id="inputField"
-                            value={this.state.userObj.DateOfBirth}
-                            onChange={(event) => { this.handleChange(event.target.value, "DateOfBirth") }}></input> */}
+class User extends Component {
+    constructor(props) {
+        super(props)
 
-                        <br />
-                        <br />
-                        <button onClick={this.clickHandler}>Save</button>
-                    </div>
-                    <br />
-                    <br />
-                    <br />
-                    {this.state.userList.length != 0 && <div><UserLists arr={this.state.userList} /> </div>}
-                </div>
+        this.state = {
+            userList: [],
+            userObj: {}
 
 
-            )
         }
     }
 
-    export default User
+    clickHandler = (event) => {
+        this.state.userList.push(this.state.userObj);
+        this.setState({
+            userObj: {}
+        })
+
+    }
+    // clickHandler = (event) => {
+    //   let tempArr=[]
+    //   tempArr.push(this.state.userObj)
+    //     this.setState ({
+    //         userList :this.state.userList.push(tempArr[0]),
+    //         userObj :{}
+            
+            
+    //     })
+    // }
+    
+
+    handleChange = (event, name) => {
+        this.state.userObj[name] = event
+    }
+
+    render() {
+        return (
+            <div >
+                <div>
+                    <label>FirstName  </label>
+                    <input type="text" name="firstName"
+                        id="inputField"
+                        value={this.state.userObj.firstName}
+                        onChange={(event) => { this.handleChange(event.target.value, "firstName") }}></input>
+                    <label>LastName  </label>
+                    <input type="text" name="lastName"
+                        id="inputField"
+                        value={this.state.userObj.lastName}
+                        onChange={(event) => { this.handleChange(event.target.value, "lastName") }}></input>
+                </div><br />
+                <div>
+
+                    <label>EmailId</label>
+                    <input type="email" name="Email"
+                        id="inputField"
+                        value={this.state.userObj.Email}
+                        onChange={(event) => { this.handleChange(event.target.value, "Email") }}></input>
+                    <label>DateOfBirth  </label>
+                    <input type="date"
+                    id="inputField"
+                    value={this.state.userObj.DateOfBirth}
+                     onChange={(event)=> { this.handleChange (event.target.value ,"DateOfBirth")}}></input>
+                    <br />
+                    <br />
+                    <button onClick={this.clickHandler}>Save</button>
+                </div>
+                <br />
+                <br />
+                <br />
+                {this.state.userList.length != 0 && <div><UserLists arr={this.state.userList} /> </div>}
+            </div>
+
+
+        )
+    }
+}
+
+export default User
